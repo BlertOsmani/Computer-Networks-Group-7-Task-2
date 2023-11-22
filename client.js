3b4802ed4bf957e4b9f65e80ea45445497b79ae2
bl3rt4 <blerta.aazemi@gmail.com>
2023-11-21 17:41:40 -0800

    basic client

--- client.js
+++ client.js
@@ -1,32 +1,38 @@
 const dgram = require('dgram');
 
 // Set server IP address and server socket port number
-const serverName = 'localhost'; // Could be IP address or host name of the server (website)
+const serverName = '10.11.65.138'; // Could be IP address or host name of the server (website)
 const serverPort = 1234; // integer
 
 // Create client socket
 const clientSocket = dgram.createSocket('udp4'); // IPv4, UDP socket
 
-// Creating message
-const message = 'Enter lower case sentence: ';
+// Function to send a message to the server
+function sendMessage() {
+    // Read user input from the console
+    process.stdout.write('Enter a message: ');
+    process.stdin.once('data', (input) => {
+        const message = input.toString().trim();
 
-// Send the message
-clientSocket.send(message, serverPort, serverName, (err) => {
-    if (err) {
-        console.error('Error sending message to server:', err);
-        clientSocket.close();
-    } else {
-        console.log(`Message sent to server at ${serverName}:${serverPort}`);
-    }
-});
+        // Send the message to the server
+        clientSocket.send(message, serverPort, serverName, (err) => {
+            if (err) {
+                console.error('Error sending message to server:', err);
+                clientSocket.close();
+            } else {
+                console.log(`Message sent to server at ${serverName}:${serverPort}`);
+            }
+        });
+    });
+}
 
 // Wait for receiving reply from Server
 clientSocket.on('message', (modifiedMessage, serverAddress) => {
     console.log('Modified Message:', modifiedMessage.toString());
     console.log('Server Address:', serverAddress);
-    
-    // Close client socket after receiving the response
-    clientSocket.close();
+
+    // Prompt user for the next message
+    sendMessage();
 });
 
 // Handle errors
@@ -38,3 +44,6 @@ clientSocket.on('error', (err) => {
 clientSocket.on('close', () => {
     console.log('Client socket is closed.');
 });
+
+// Start by sending the first message
+sendMessage();
