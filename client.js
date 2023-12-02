fdec7791683b8700c8689413ac0a75c7bda3f962
bl3rt4 <121666373+bl3rt4@users.noreply.github.com>
2023-12-02 08:54:56 -0800

    Update client.js

--- client.js
+++ client.js
@@ -46,3 +46,48 @@ function startClient() {
     });
 
     client.send('Connected', PORT, IP_ADDRESS);
+    
+
+function promptOptions() {
+  rl.question('Choose an option (read/write/delete/upload/exit): ', (option) => {
+    if (option.toLowerCase() === 'read') {
+      rl.question('Enter the file path to read: ', (filePath) => {
+        const readCommand = `read ${filePath}`;
+        client.send(readCommand, PORT, IP_ADDRESS);
+      });
+    } else if (option.toLowerCase() === 'write') {
+      rl.question('Enter the file path to write: ', (filePath) => {
+        rl.question('Enter the content to write: ', (content) => {
+          const writeCommand = `write ${filePath} "${content}"`;
+          client.send(writeCommand, PORT, IP_ADDRESS);
+        });});
+    } else if (option.toLowerCase() === 'delete') {
+      rl.question('Enter the file path to delete: ', (filePath) => {
+        const deleteCommand = `delete ${filePath}`;
+        client.send(deleteCommand, PORT, IP_ADDRESS);
+      });
+    } else if (option.toLowerCase() === 'upload') {
+      rl.question('Enter the file path to upload: ', (filePath) => {
+        rl.question('Enter the content to upload: ', (content) => {
+          const uploadCommand = `upload ${filePath} "${content}"`;
+          client.send(uploadCommand, PORT, IP_ADDRESS);
+        });
+      });
+    } else if (option.toLowerCase() === 'exit') {
+      client.send('exit', PORT, IP_ADDRESS);
+          client.close();
+          rl.close();
+          return;
+    } else {
+      console.log('Invalid option');
+      promptOptions();
+    }
+  });
+}
+
+
+    promptOptions();
+  });
+}
+
+startClient();
